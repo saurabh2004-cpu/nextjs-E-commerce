3179433772f9e19efecf662725c174c30b3c2a73
Your Name <saurabhbodakhe2004@gmail.com>
2024-09-18 19:49:58 +0530

    update api path in frontend :current user

--- src/app/(frontend)/services/api.ts
+++ src/app/(frontend)/services/api.ts
@@ -1,19 +1,10 @@
 import axios from 'axios';
-import https from 'https'; // Node.js 'https' module
 
-// Check if the code is running on the server-side (Node.js)
-const isServer = typeof window === 'undefined';
-
-// Create the axios instance
 const axiosInstance = axios.create({
   baseURL: 'https://localhost:3000',
   withCredentials: true, // To send cookies with requests
-  ...(isServer && {
-    httpsAgent: new https.Agent({
-      rejectUnauthorized: false, // Ignore SSL issues in development
-    }),
-  }),
+  
 });
 
-// Export the axios instance for use throughout your app
+
 export default axiosInstance;
