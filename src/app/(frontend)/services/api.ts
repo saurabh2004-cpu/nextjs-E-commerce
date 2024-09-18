975293fa0ce50c4f60426f88a90f546ef101c63b
Your Name <saurabhbodakhe2004@gmail.com>
2024-09-18 19:13:18 +0530

    update the http to https in services

--- src/app/(frontend)/services/api.ts
+++ src/app/(frontend)/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://localhost:3000',
   withCredentials: true, // To send cookies with requests
 });
 
