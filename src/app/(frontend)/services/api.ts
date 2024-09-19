66bdfd3d8806f38fc32f2a8775a64eb718d638fb
Your Name <saurabhbodakhe2004@gmail.com>
2024-09-19 17:45:40 +0530

    update base url path

--- src/app/(frontend)/services/api.ts
+++ src/app/(frontend)/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://localhost:3000',
+  // baseURL: 'http://localhost:3000',
   baseURL: 'https://nextjs-e-commerce-2.vercel.app',
   withCredentials: true, // To send cookies with requests
   
